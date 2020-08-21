e47759b3e8d4eeec60951a45edfd86ca5025b22e
Abderrahmen MCHAYA <abderrahmen.mchayaa@gmail.com>
2020-08-21 20:05:48 +0100

    create a popUp with js

---
+++ myScript.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    setTimeout(function () {
+        popUp('show');
+    }, 1000);
+}
+
+function popUp() {
+    window.alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")
+}
+
+function goToWebsite() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
