7d8143f09bf214476470446235808af21ed933cc
Abderrahmen MCHAYA <abderrahmen.mchayaa@gmail.com>
2020-08-21 23:36:56 +0100

    add a popUp tp the site but scriptin html

--- myScript.js
+++ myScript.js
@@ -1,14 +1,1 @@
-window.onload = function () {
-    setTimeout(function () {
-        popUp('show');
-    }, 1000);
-}
-
-function popUp() {
-    window.alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")
-}
-
-function goToWebsite() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-}
 
