ecc68fcd6c8106137f62e5b0fc3976972f60b5df
Abderrahmen MCHAYA <abderrahmen.mchayaa@gmail.com>
2020-08-22 23:06:26 +0100

    make the script in an external file

--- myScript.js
+++ myScript.js
@@ -1,1 +1,24 @@
+var win = document.getElementById("popUp");
 
+var closeWin = document.getElementById("close_popUp");
+
+var winText = document.getElementById("popUp_text");
+
+
+window.onload = function () {
+    setTimeout(function () {
+        win.style.display = "none";
+    }, 10000);
+}
+
+// When the user clicks on <span> (x), close the modal
+closeWin.onclick = function () {
+    win.style.display = "none";
+
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == win) {
+        win.style.display = "none";
+    }
+}
